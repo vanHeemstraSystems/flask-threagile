c1eeebcd353272f31433e7b816abd5a4242d3242
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-01 10:50:27 +0200

    Update models.py

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,1 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    ## Stage 1: Plain Text
+    #
+
+    ## Stage 2: Encryption
+    #
+  
+    ## Stage 3: Hashing
+    #
+  
+    ## Stage 4: Hashing and Salting
+    #
+  
+    ## Stage 5: BCrypt  
+    password_hash = db.Column(db.String(128), nullable=False) # Store hashed password
+
+def __repr__(self):
+return f'<User {self.username}>'
