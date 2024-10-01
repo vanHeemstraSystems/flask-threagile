00d35aba3075eab8479da449889a50e440a3c459
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-01 10:59:40 +0200

    Update models.py

--- flask_app/models.py
+++ flask_app/models.py
@@ -9,19 +9,19 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     ## Stage 1: Plain Text
-    #
+    # 
 
     ## Stage 2: Encryption
-    #
+    # password = db.Column(db.LargeBinary, nullable=False)  # Store encrypted password
   
     ## Stage 3: Hashing
-    #
+    # 
   
     ## Stage 4: Hashing and Salting
-    #
+    # 
   
     ## Stage 5: BCrypt  
-    password_hash = db.Column(db.String(128), nullable=False) # Store hashed password
+    # password_hash = db.Column(db.String(128), nullable=False) # Store hashed password
 
 def __repr__(self):
 return f'<User {self.username}>'
