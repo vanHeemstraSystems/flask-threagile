207c9296f2e533603b048c0f4bc57bcf5bcef182
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-01 11:32:12 +0200

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
-import bcrypt
+import bcrypt # Used for bcrypt
+from cryptography.fernet import Fernet # Used for encryption
 from flask import Flask, request, jsonify, render_template, redirect, url_for 
 from models import db, User
 
